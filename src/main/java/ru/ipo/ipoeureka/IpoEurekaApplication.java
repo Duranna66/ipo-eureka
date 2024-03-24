d29644d0837dbdfff3d7235960ac0b33d1570926
Николаев Иван Сергеевич <ivan.kushnarev.04@list.ru>
2024-03-24 17:42:46 +0300

    init

---
+++ src/main/java/ru/ipo/ipoeureka/IpoEurekaApplication.java
@@ -0,0 +1,15 @@
+package ru.ipo.ipoeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class IpoEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpoEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ipo/ipoeureka/IpoEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ipo.ipoeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpoEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
